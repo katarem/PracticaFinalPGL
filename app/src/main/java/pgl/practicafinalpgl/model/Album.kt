c6420a04e3da60c4a197f2f2655deebabff4e5f6
ChristianGomez <57232755+ChrGomz@users.noreply.github.com>
2024-02-04 20:42:13 +0000

    Cambios a class por compatibilidad
    song y album ahora son class

--- app/src/main/java/pgl/practicafinalpgl/model/Album.kt
+++ app/src/main/java/pgl/practicafinalpgl/model/Album.kt
@@ -1,5 +1,15 @@
 package pgl.practicafinalpgl.model
 
-data class Album(val id: String?, val name: String?, val songs: List<Song>?, val releaseDate: String?, val artistId: String?, val portait: String?): Entity(){
-    constructor(id: String) : this(id,null,null,null,null,null)
+class Album(
+    val id: String?,
+    val name: String?,
+    val songs: List<Song>?,
+    val releaseDate: String?,
+    val artistId: String?,
+    val portait: String?
+) : Entity() {
+
+    constructor() : this(null, null, null, null, null, null)
+
+    constructor(id: String) : this(id, null, null, null, null, null)
 }
--- app/src/main/java/pgl/practicafinalpgl/model/Song.kt
+++ app/src/main/java/pgl/practicafinalpgl/model/Song.kt
@@ -1,5 +1,13 @@
 package pgl.practicafinalpgl.model
 
-data class Song(val id: String?, val name: String?, val artistId: String?, val albumId: String?): Entity(){
-    constructor(id: String): this(id,null,null,null)
+class Song(
+    val id: String?,
+    val name: String?,
+    val artistId: String?,
+    val albumId: String?
+) : Entity() {
+
+    constructor() : this(null, null, null, null)
+
+    constructor(id: String) : this(id, null, null, null)
 }
