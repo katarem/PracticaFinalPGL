f8b9c49ed8ac9c07f6f60bc638b032c36d685ea0
katarem <crismigesp@gmail.com>
2024-02-05 23:44:27 +0000

    U Se me pasó admin

--- app/src/main/java/pgl/practicafinalpgl/model/login/Admin.kt
+++ app/src/main/java/pgl/practicafinalpgl/model/login/Admin.kt
@@ -1,22 +1,8 @@
 package pgl.practicafinalpgl.model.login
 
-import java.util.ArrayList
-
-
-
-
-class Admin : UserType {
+class Admin() : UserType() {
     override var permissions: Permissions = Permissions.ADMIN
-
-    constructor(): super()
-    constructor(id: String): super(id,Permissions.ADMIN)
-    constructor(
-        nickname: String,
-        profilePhoto: String,
-        ownedPlaylists: ArrayList<String>,
-        following: Int,
-        followers: Int
-    ): super(nickname, profilePhoto, ownedPlaylists, following, followers, Permissions.ARTIST)
-
-
+    constructor(id: String): this(){
+        this.id = id
+    }
 }
